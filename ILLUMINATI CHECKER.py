96b92dd58256268af3d6432168ed0fcf198153b2
qwerty36 <richard@lrtbl.nl>
2014-12-06 16:30:51 +0100

    Illuminati checker (ALPHA)

---
+++ ILLUMINATI CHECKER.py
@@ -0,0 +1,16 @@
+##############################################################
+###ILLUMINATI MEMBER CHECKER, By: Richard Jansen, 6-12-2014###
+##############################################################
+
+code = open ("code.txt")
+HAX = str(input("WH0 D0 Y0U 5U5P3CT T0 83 1LLUM1N4T1??? "))
+
+print (str(code.readline()))
+print (" ")
+print (" ")
+
+if len(HAX) > 1:
+    print ("0m6 dud3, 7h47 p3r50n 15 d3f1n473ly 1llum1n471 l337 h4xx0r, y0u b3 c4r3full n0w!!! ")
+
+else:
+    print ("7h47 41n7 n0 n4m3 ")
