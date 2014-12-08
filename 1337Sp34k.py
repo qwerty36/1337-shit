62ad6bf86f15fe037052bf0ffcec9140bc2f951a
Richard Jansen <richard@lrtbl.nl>
2014-12-08 14:19:56 +0100

    new shizzle

---
+++ 1337Sp34k.py
@@ -0,0 +1,3 @@
+x = str(input("Insert your text here "))
+
+print (x.replace("a","4").replace("i","1").replace("e","3").replace("l","7").replace("o","0"))
---
+++ basic clock.py
@@ -0,0 +1,30 @@
+#############################################################################################
+##Basic Clock application with a GUI, By Richard Jansen, HAN University of Applied Sciences##
+##4-12-2014##################################################################################
+#############################################################################################
+
+######################
+##Local Time Import###
+######################
+
+import tkinter as tk
+import time
+
+##########################
+##Application Variables###
+##########################
+
+class App():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.label = tk.Label(text="")
+        self.label.pack()
+        self.update_clock()
+        self.root.mainloop()
+        
+    def update_clock(self):
+        now = time.strftime("%H:%M:%S")
+        self.label.configure(text=now)
+        self.root.after(1000, self.update_clock)
+
+app=App()
