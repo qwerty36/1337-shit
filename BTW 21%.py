dcdd1d518579af0a3909d1d931797c525a83d4e9
qwerty36 <richard@lrtbl.nl>
2015-01-20 20:26:15 +0100

    BTW calculator

---
+++ BTW 21%.py
@@ -0,0 +1,8 @@
+#####################################################################
+##Simple Application to deduct taxes with the dutch 6% tax rating####
+##Richard Jansen HAN University of Applied Sciences Nijmegen ########
+#####################################################################
+x = float(input("Bedrag inclusief 6% BTW ? "))
+y = (x)/(1.06)
+print (" ")
+print (y)
---
+++ BTW 6%.py
@@ -0,0 +1,8 @@
+#####################################################################
+##Simple Application to deduct taxes with the dutch 21% tax rating###
+##Richard Jansen HAN University of Applied Sciences Nijmegen ########
+#####################################################################
+x = float(input("Bedrag inclusief 21% BTW ? "))
+y = (x)/(1.21)
+print (" ")
+print (y)
